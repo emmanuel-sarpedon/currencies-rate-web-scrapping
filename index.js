4021ba663ef3a1f5bcac07245c6c64992543ed85
Emmanuel SARPEDON <emmanuel.sarpedon@gmail.com>
2021-08-10 16:28:04 +0400

    [FEAT]: prevent parallel processes
    prevent parallel processes to run if update is already in progress
    save RAM memory

--- index.js
+++ index.js
@@ -48,99 +48,110 @@ const getRateFromUrl = async (url) => {
   return rate; // on renvoie le taux affiché sur le site
 };
 
+let isUpdating = false;
+
 app.get("/update", (req, res) => {
-  try {
-    (async () => {
-      // Lancement du navigateur
+  if (!isUpdating) {
+    isUpdating = true;
+    try {
+      (async () => {
+        // Lancement du navigateur
 
-      console.log("Initialisation de Puppeteer...");
-      const browser = await puppeteer.launch({
-        headless: true,
-        args: [
-          "--incognito",
-          "--no-sandbox",
-          "--single-process",
-          "--no-zygote",
-        ],
-      });
-      const page = await browser.newPage();
-      await page.goto(process.env.SOURCE_URL);
+        console.log("Initialisation de Puppeteer...");
+        const browser = await puppeteer.launch({
+          headless: true,
+          args: [
+            "--incognito",
+            "--no-sandbox",
+            "--single-process",
+            "--no-zygote",
+          ],
+        });
+        const page = await browser.newPage();
+        await page.goto(process.env.SOURCE_URL);
 
-      // Récupération de toutes les conversions possibles ainsi que les url
-      const listOfCurrencies = await page.evaluate(() => {
-        const el = Array.from(document.querySelectorAll("td a"));
+        // Récupération de toutes les conversions possibles ainsi que les url
+        const listOfCurrencies = await page.evaluate(() => {
+          const el = Array.from(document.querySelectorAll("td a"));
 
-        const list = el.map((el, i) => ({
-          id: i,
-          from: {
-            currency: el.innerHTML.slice(3, 6),
-            description: el.innerHTML.slice(12, 16),
-          },
-          to: {
-            currency: el.innerHTML.slice(31, 34),
-            description: el.innerHTML.slice(40, el.innerHTML.length - 1),
-          },
-          link: el.href,
-        }));
-
-        return list;
-      });
-
-      await browser.close();
-
-      console.log("Listing des devises terminées - Début de la mise à jour");
-
-      return listOfCurrencies;
-    })() // Attente de la résolution de la promesse browser.close()
-      .then(async (currencies) => {
-        const currenciesUpdated = currencies;
-
-        // Mise à jour du taux pour chaque devise présente dans l'array 'result'
-        for (let i = 0; i < currenciesUpdated.length; i++) {
-          try {
-            currenciesUpdated[i].rate = await getRateFromUrl(
-              currenciesUpdated[i].link
-            );
-            currenciesUpdated[i].update = Date();
-          } catch (error) {
-            currenciesUpdated[i].rate = undefined;
-            currenciesUpdated[i].update = undefined;
-          }
-
-          // Création d'une nouvelle devise et sauvegarde dans la base de données MongoDB
-          const newCurrency = new Currency({
+          const list = el.map((el, i) => ({
+            id: i,
             from: {
-              currency: currenciesUpdated[i].from.currency,
-              description: currenciesUpdated[i].from.description,
+              currency: el.innerHTML.slice(3, 6),
+              description: el.innerHTML.slice(12, 16),
             },
             to: {
-              currency: currenciesUpdated[i].to.currency,
-              description: currenciesUpdated[i].to.description,
+              currency: el.innerHTML.slice(31, 34),
+              description: el.innerHTML.slice(40, el.innerHTML.length - 1),
             },
-            link: currenciesUpdated[i].link,
-            rate: currenciesUpdated[i].rate,
-            update: currenciesUpdated[i].update,
-          });
+            link: el.href,
+          }));
 
-          if (newCurrency.rate) {
-            // si on ne parvient pas à récupérer le taux on ne modifie pas la BDD
-            await newCurrency.save();
+          return list;
+        });
+
+        await browser.close();
+
+        console.log("Listing des devises terminées - Début de la mise à jour");
+
+        return listOfCurrencies;
+      })() // Attente de la résolution de la promesse browser.close()
+        .then(async (currencies) => {
+          const currenciesUpdated = currencies;
+
+          // Mise à jour du taux pour chaque devise présente dans l'array 'result'
+          for (let i = 0; i < currenciesUpdated.length; i++) {
+            try {
+              currenciesUpdated[i].rate = await getRateFromUrl(
+                currenciesUpdated[i].link
+              );
+              currenciesUpdated[i].update = Date();
+            } catch (error) {
+              currenciesUpdated[i].rate = undefined;
+              currenciesUpdated[i].update = undefined;
+            }
+
+            // Création d'une nouvelle devise et sauvegarde dans la base de données MongoDB
+            const newCurrency = new Currency({
+              from: {
+                currency: currenciesUpdated[i].from.currency,
+                description: currenciesUpdated[i].from.description,
+              },
+              to: {
+                currency: currenciesUpdated[i].to.currency,
+                description: currenciesUpdated[i].to.description,
+              },
+              link: currenciesUpdated[i].link,
+              rate: currenciesUpdated[i].rate,
+              update: currenciesUpdated[i].update,
+            });
+
+            if (newCurrency.rate) {
+              // si on ne parvient pas à récupérer le taux on ne modifie pas la BDD
+              await newCurrency.save();
+            }
+
+            // Affichage de l'avancement de la mise à jour dans la console
+            console.log(
+              (((i + 1) / currenciesUpdated.length) * 100).toFixed(2) +
+                "% : " +
+                (i + 1) +
+                " / " +
+                currenciesUpdated.length +
+                " traités" +
+                isUpdating
+            );
+
+            i === currenciesUpdated.length - 1 && (isUpdating = false); // on passe la variable isUpdating à false lorsque toute la màj est terminée
           }
-
-          console.log(
-            (((i + 1) / currenciesUpdated.length) * 100).toFixed(2) +
-              "% : " +
-              (i + 1) +
-              " / " +
-              currenciesUpdated.length +
-              " traités"
-          );
-        }
-      });
-
-    res.status(200).json({ message: "update in progress" });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
+        });
+      res.status(200).json({ message: "update in progress" });
+    } catch (error) {
+      isUpdating = false;
+      res.status(400).json({ error: error.message });
+    }
+  } else {
+    res.status(200).json({ message: "update already in progress" });
   }
 });
 
